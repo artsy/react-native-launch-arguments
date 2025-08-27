59036a74a010aa83550b3684f64ecea33d467dac
Mounir Dhahri <soussouheros@gmail.com>
2025-08-27 12:51:03 +0200

    chore: bring old architecture support

--- android/src/main/java/com/reactnativelauncharguments/LaunchArgumentsPackage.java
+++ android/src/main/java/com/reactnativelauncharguments/LaunchArgumentsPackage.java
@@ -1,19 +1,23 @@
 package com.reactnativelauncharguments;
 
 import com.facebook.react.BaseReactPackage;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
 import com.facebook.react.TurboReactPackage;
+import com.facebook.react.uimanager.ViewManager;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public class LaunchArgumentsPackage extends TurboReactPackage {
+public class LaunchArgumentsPackage extends TurboReactPackage implements ReactPackage {
 
     @Nullable
     @Override
@@ -43,5 +47,16 @@ public class LaunchArgumentsPackage extends TurboReactPackage {
                     ));
             return moduleInfos;
         };
+    }
+
+    // ReactPackage methods for old architecture support
+    @Override
+    public List<NativeModule> createNativeModules(@Nonnull ReactApplicationContext reactContext) {
+        return Collections.<NativeModule>singletonList(new LaunchArgumentsModule(reactContext));
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(@Nonnull ReactApplicationContext reactContext) {
+        return Collections.emptyList();
     }
 }
