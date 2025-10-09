06170ea8f32ca2a04c59cf3ba9da850503404213
Mounir Dhahri <soussouheros@gmail.com>
2025-10-09 11:07:09 +0200

    chore: upgrade packages to fix types (#15)

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ const LINKING_ERROR =
   "- You rebuilt the app after installing the package\n" +
   "- You are not using Expo managed workflow\n";
 
-// @ts-expect-error
 const isTurboModuleEnabled = global.__turboModuleProxy != null;
 
 const LaunchArgumentsModule = isTurboModuleEnabled
